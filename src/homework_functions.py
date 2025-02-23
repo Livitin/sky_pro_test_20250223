0faaafbaf39209ab25dcac08bbda3e1dde2a7dbb
Livitin Serg <livitin@itprofservice.ru>
2025-02-23 23:00:11 +0300

    made selected_lists_by_state

--- src/homework_functions.py
+++ src/homework_functions.py
@@ -2,7 +2,12 @@ def selected_lists_by_state(passed_lists: list, state:str = 'EXECUTED') -> list:
     '''Функция принимает на вход список словарей с данными о банковских операциях и параметр
     state , возвращает новый список, содержащий только те словари, у которых ключ state
     содержит переданное в функцию значение'''
-    pass
+    result = []
+    for tmp_list in passed_lists:
+        if state in tmp_list and tmp_list['state'] == state:
+            result.append(tmp_list)
+    return result
+
 
 def sort_lists(passed_lists: list, sort_by_date:bool = 'True') -> list:
     '''Функция принимает на вход список словарей и параметр порядка сортировки,
