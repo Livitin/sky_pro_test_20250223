db277719e8b3b376b88b6f1f331c269cb10dcbd3
Livitin Serg <livitin@itprofservice.ru>
2025-02-23 23:15:43 +0300

    made refactor corrections to solve homework. Created processing, moved file with procedures.

--- src/processing/homework_functions.py
+++ src/processing/homework_functions.py
@@ -1,4 +1,6 @@
-def selected_lists_by_state(passed_lists: list, state:str = 'EXECUTED') -> list:
+from datetime import datetime
+
+def filter_by_state(passed_lists: list, state:str = 'EXECUTED') -> list:
     '''Функция принимает на вход список словарей с данными о банковских операциях и параметр
     state , возвращает новый список, содержащий только те словари, у которых ключ state
     содержит переданное в функцию значение'''
@@ -9,8 +11,10 @@ def selected_lists_by_state(passed_lists: list, state:str = 'EXECUTED') -> list:
     return result
 
 
-def sort_lists(passed_lists: list, sort_by_date:bool = 'True') -> list:
+def sort_by_date(passed_lists: list, sort_by_date_forward:bool = 'True') -> list:
     '''Функция принимает на вход список словарей и параметр порядка сортировки,
     возвращает новый список, в котором исходные словари отсортированы по дате.
     Порядок направления сортировки по умолчанию True'''
-    pass
+    result = []
+    tmp_date:datetime = datetime.now()
+    list_index =
