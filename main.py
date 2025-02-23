7eba8ca1accc720ac123109510c4c7a41107a38c
Livitin Serg <livitin@itprofservice.ru>
2025-02-23 23:46:17 +0300

    Make tests for second procedure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.processing.homework_functions import filter_by_state
-from src.processing.homework_functions sort_by_date
+from src.processing.homework_functions import sort_by_date
 
 print('Hello! Let\'s try to make operations')
 
@@ -8,9 +8,22 @@ test_lists = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29
               {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
               {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
               ]
-
+''' First test with 'state' by default '''
 print(filter_by_state(test_lists))
+
+''' Second test when 'state' is 'EXECUTED' '''
 print(filter_by_state(test_lists),'EXECUTED')
+
+''' The third test with other state'''
 print(filter_by_state(test_lists),'CANCELED')
+
+
+''' Try to sort lists by default (True) '''
+print(sort_by_date(test_lists))
+
+''' Try to sort lists with sort option (for example False) '''
+print(sort_by_date(test_lists,'False'))
+
+
 
 
